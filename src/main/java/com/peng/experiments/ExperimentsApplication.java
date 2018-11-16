6a8b9483ab5c39d99f163170249957de917f4260
郭清朋mac <gpl_family@163.com>
2018-11-17 01:08:16 +0800

    Initial commit

---
+++ src/main/java/com/peng/experiments/ExperimentsApplication.java
@@ -0,0 +1,12 @@
+package com.peng.experiments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * created by guoqingpeng on 2018/11/17
+ */
+@SpringBootApplication
+public class ExperimentsApplication {
+    public static void main(String[] args) { SpringApplication.run(ExperimentsApplication.class,args); }
+}
