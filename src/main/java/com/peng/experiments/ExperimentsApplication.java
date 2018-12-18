86a67d4e8fe65dfb981e2f4dfa2781d16c991afa
郭清朋mac <gpl_family@163.com>
2018-12-18 21:45:27 +0800

    升级springboot2.0.1,添加打包工具assembly

--- src/main/java/com/peng/experiments/ExperimentsApplication.java
+++ src/main/java/com/peng/experiments/ExperimentsApplication.java
@@ -1,12 +1,22 @@
 package com.peng.experiments;
 
+import ch.qos.logback.core.db.DataSourceConnectionSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * created by guoqingpeng on 2018/11/17
  */
-@SpringBootApplication
+@SpringBootApplication //(exclude = {DataSourceConnectionSource.class})
+@RestController
 public class ExperimentsApplication {
+
     public static void main(String[] args) { SpringApplication.run(ExperimentsApplication.class,args); }
+
+    @GetMapping("/test")
+    public String test(){
+        return "hello word";
+    }
 }
